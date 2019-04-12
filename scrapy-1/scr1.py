9b33c584f6feb75179d7ad2430e1a2123c614918
mykespb <mykespb@gmail.com>
2019-04-12 12:02:11 +0300

    better desc

--- scrapy-1/scr1.py
+++ scrapy-1/scr1.py
@@ -2,7 +2,9 @@
 # Mikhail Kolodin, 2019
 # python3, user virtualenv
 # text from docs to make futher tests
-# taken from scrapinghub.com 
+# taken from scrapy.org, scrapinghub.com 
+# https://www.youtube.com/watch?v=CsaqVQ4NIEU 
+# youtube: Scrapy - Overview and Demo (web crawling and scraping) by Melvin L
 
 import scrapy
 
