826cba92266528ac8a865179e83c92d62193f6d3
mykespb <mykespb@gmail.com>
2019-04-12 17:01:17 +0300

    good results in 3 cases

--- selenium-test/README.md
+++ selenium-test/README.md
@@ -8,10 +8,22 @@ selenium installed,
 webdrivers for Chrome 73 and Firefox installed,
 Chrome updated to ver. 73.
 
-Problems
+Problems solved
 --------------------------------
 
-drivers do not work, 
-complaining to local ports protection.
+We need to call it like:
 
+python ./file.py 
+
+Results
+--------------------------------
+
+selenium_using_chrome.py
+ok
+
+sel-open-1.py
+ok
+
+sel-test-2.py
+ok
 
--- selenium-test/sel-open-1.py
+++ selenium-test/sel-open-1.py
@@ -1,18 +1,26 @@
 # myke tests selenium
-# 2019-04-12 v.0.2
+# 2019-04-12 v.1.0
 
 # YouTube Video: https://www.youtube.com/watch?v=eDrFWRi13DY
 
 from selenium import webdriver
+# ~ options = webdriver.ChromeOptions()
 
 # Old way of doing things that should work with Firefox, but soesn.t
 # ~ driver = webdriver.Firefox()
 # ~ driver.get("http://google.com")
 
 # Another way of doing things that should work with Chrome, but doesn't'
-chromedriver = "chromedriver"
-driver = webdriver.Chrome(chromedriver)
-driver.get("https://google.com")
+#chromedriver = "chromedriver"
+#driver = webdriver.Chrome(chromedriver)
+#driver = webdriver.Chrome()
+
+mysite = r"https://google.com"
+driver = webdriver.Chrome()
+
+# ~ driver = webdriver.Remote(site, webdriver.DesiredCapabilities.HTMLUNIT.copy())
+
+driver.get(mysite)
 
 #site = r"http://www.myke.spb.ru"
 
